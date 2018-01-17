7c887a32e94cffdc77e59c2467ab211b7d3e7ef3
ngammoudi <klmnitts1980>
2018-01-17 17:23:28 +0100

    update pipeline project

--- src/test/java/ui/FindFlightsUITest.java
+++ src/test/java/ui/FindFlightsUITest.java
@@ -1,25 +1,55 @@
 package ui;
 
+import java.net.URL;
+
 import net.serenitybdd.junit.runners.SerenityRunner;
-import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.Steps;
-import net.thucydides.core.annotations.WithTag;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import steps.ui.FindFlightsUISteps;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SerenityRunner.class)
 public class FindFlightsUITest {
-    @Managed
-    WebDriver driver;
 
-    @Steps
-    FindFlightsUISteps findFlightsUISteps;
+    private static Wait<WebDriver> wait;
+    private static DesiredCapabilities capabillities;
+    private static WebDriver driver;
+    @BeforeClass
+    public static void setUp() throws Exception {
+        capabillities = DesiredCapabilities.chrome();
+        /** URL is the selenium hub URL here **/
+        driver = new RemoteWebDriver(new URL("http://172.17.0.2:4444/wd/hub"), capabillities);
+        capabillities.setBrowserName("chrome");
+        wait = new WebDriverWait(driver, 6000);
+    }
+    /**
+     * To test the UI
+     * @throws Exception
+     */
+    public void testUI() throws Exception {
+        /** Your application URL which you want to test **/
+        driver.get("http://localhost:8090/myapp/index.html");
+        wait.until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver webDriver) {
 
-    @Test
-    @WithTag(type="type", name="UI")
-    public void  findFlights() {
-        findFlightsUISteps.findFlights();
+                return webDriver.findElement(By.tagName("title")) != null;
+            }
+        });
+
+        assertEquals("URL must be http://localhost:8090/myapp/index.html", "http://localhost:8090/myapp/index.html", driver.getCurrentUrl());
+        /** put other asserts as well **/
+    }
+    @AfterClass
+    public static void tearDown() throws Exception {
+        driver.quit();
     }
 }
