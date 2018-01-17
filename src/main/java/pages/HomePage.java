38052de0a49c7ecbf57750afafc9ac4137c3d099
ngammoudi <klmnitts1980>
2018-01-17 17:10:13 +0100

    update pipeline project

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,26 +4,13 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
 
 public class HomePage extends PageObject {
     @FindBy(xpath= "//input[contains(@value,'Find Flights')]")
     public WebElement findFlightsButton;
 
-    public HomePage(RemoteWebDriver driver) {
+    public HomePage(WebDriver driver) {
         super(driver);
-        DesiredCapabilities dc = DesiredCapabilities.chrome();
-        try {
-            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
         open();
     }
 
